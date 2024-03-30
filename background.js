b720b4b78f05f799c7b9a4bd05a64810c94b0433
Michael Gao <mgao31415@gmail.com>
2024-03-30 14:58:24 -0400

    figured out how to get html

--- background.js
+++ background.js
@@ -5,12 +5,10 @@ chrome.action.onClicked.addListener((tab) => {
       target: {tabId: tab.id},
       files: ['content.js']
     });
-  });  
+  });
 
 chrome.runtime.onMessage.addListener((message) => {
-    // Store the scraped HTML content in local storage
-    chrome.storage.local.set({ 'scrapedHTML': message.html }, function() {
-        // Log a message to indicate successful storage
-        console.log('Scraped HTML content stored in local storage.');
+    chrome.storage.local.set({"scrapedHTML": message.html}, function() {
+        console.log(message.html);
     });
 });
--- content.js
+++ content.js
@@ -1,13 +1,7 @@
-// content.js
+function ScrapeHTML() {
+    const htmlcontent = document.documentElement.innerHTML;
 
-// Function to scrape HTML and send it to background script
-function scrapeHTML() {
-    // Get the HTML content of the current webpage
-    const htmlContent = document.documentElement.outerHTML;
-    
-    // Send the HTML content to background script
-    chrome.runtime.sendMessage({ html: htmlContent });
+    chrome.runtime.sendMessage({html: htmlcontent });
 }
 
-// Execute the scrapeHTML function when the extension button is clicked
-scrapeHTML();
+ScrapeHTML();
